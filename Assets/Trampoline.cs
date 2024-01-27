d646ffe4fda74d2027f7aa7e11519ad03bf3f029
Lorenzo Neri <hello@lorenzoneri.com>
2024-01-27 10:33:40 +0100

    basic trampoline

---
+++ Assets/Trampoline.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Trampoline : MonoBehaviour {
+	public float springForce = 1000;
+	private Collision2D collision;
+	private bool bouncing = false;
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (!bouncing) {
+			bouncing = true;
+			collision = coll;
+		}
+	}
+
+	void FixedUpdate () {
+		if (bouncing) {
+			var rb = collision.gameObject.GetComponent<Rigidbody2D> ();
+			rb.velocity = new Vector3 (0, 0, 0);
+			rb.AddForce (new Vector2 (0f, springForce));
+			bouncing = false;
+		}
+	}
+}
