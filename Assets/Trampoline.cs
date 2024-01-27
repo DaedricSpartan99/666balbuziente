e20aafafe9f2d7c71a8821bd2ece5cbbad2702c7
bluwolfer777 <leonleonleonr@gmail.com>
2024-01-27 11:43:21 +0100

    Trampoline toggle

--- Assets/Trampoline.cs
+++ Assets/Trampoline.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.VisualScripting;
 public class Trampoline : MonoBehaviour {
 	public float springForce = 1000;
 	private Collision2D collision;
 	private bool bouncing = false;
+	private bool? clickStatus = false;
 
 	void OnCollisionEnter2D(Collision2D coll) {
 		if (!bouncing) {
@@ -13,8 +15,16 @@ public class Trampoline : MonoBehaviour {
 		}
 	}
 
+	void Update() {
+		if (Input.GetMouseButtonDown(0) && gameObject.name == "Trampolino"){
+			Debug.Log((Variables.Object(gameObject).Get("clickStatus")) as bool?);
+			clickStatus = !(Variables.Object(gameObject).Get("clickStatus") as bool?);
+			Variables.Application.Set("clickStatus",clickStatus);
+   		}
+	}
+
 	void FixedUpdate () {
-		if (bouncing) {
+		if (bouncing && ((Variables.Object(gameObject).Get("clickStatus") as bool?) == true)) {
 			var rb = collision.gameObject.GetComponent<Rigidbody2D> ();
 			rb.velocity = new Vector3 (0, 0, 0);
 			rb.AddForce (new Vector2 (0f, springForce));
