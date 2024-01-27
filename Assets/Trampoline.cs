2daeb3c8fab929a7ba725c28c9645aaca0e800f9
Lorenzo Neri <hello@lorenzoneri.com>
2024-01-27 12:05:32 +0100

    working and disabling/enabling trampoline

--- Assets/Trampoline.cs
+++ Assets/Trampoline.cs
@@ -6,7 +6,7 @@ public class Trampoline : MonoBehaviour {
 	public float springForce = 1000;
 	private Collision2D collision;
 	private bool bouncing = false;
-	private bool? clickStatus = false;
+	private bool clickStatus = false;
 
 	void OnCollisionEnter2D(Collision2D coll) {
 		if (!bouncing) {
@@ -14,17 +14,19 @@ public class Trampoline : MonoBehaviour {
 			collision = coll;
 		}
 	}
-
-	void Update() {
-		if (Input.GetMouseButtonDown(0) && gameObject.name == "Trampolino"){
-			Debug.Log((Variables.Object(gameObject).Get("clickStatus")) as bool?);
-			clickStatus = !(Variables.Object(gameObject).Get("clickStatus") as bool?);
-			Variables.Application.Set("clickStatus",clickStatus);
+    public void OnMouseDown(){
+        if (Input.GetMouseButtonDown(0)){
+            
+            clickStatus=!(clickStatus);
    		}
+    }
+	void Update() {
+		
 	}
 
 	void FixedUpdate () {
-		if (bouncing && ((Variables.Object(gameObject).Get("clickStatus") as bool?) == true)) {
+        
+		if (bouncing && clickStatus) {
 			var rb = collision.gameObject.GetComponent<Rigidbody2D> ();
 			rb.velocity = new Vector3 (0, 0, 0);
 			rb.AddForce (new Vector2 (0f, springForce));
