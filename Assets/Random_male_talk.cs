320ec4d716fe80203066902d3643780d3a50b639
AtlasCount97 <raffaele.ancarola@protonmail.com>
2024-01-28 12:09:09 +0100

    Person collision prefab

---
+++ Assets/Random_male_talk.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Random_male_talk : MonoBehaviour
+{
+    //public List<AudioClip> collisionSounds = new List<AudioClip>();
+
+    public AudioClip[] collisionSounds;
+    public float audioScale = 1.0f;
+
+    AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Player")) {
+          source.PlayOneShot(collisionSounds[Random.Range(0, collisionSounds.Length)], audioScale);
+        }
+    }
+}
