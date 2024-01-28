24b3d6d3adbd06ebc931545471311518cd85f19a
Zeno <zeno@ThinkPad-T14-Gen-2i-afcb3469.localdomain>
2024-01-28 11:58:42 +0100

    roba per pascquale

--- Assets/Scripts/SpegniTutto.cs
+++ Assets/Scripts/SpegniTutto.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class SpegniTutto : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Application.Quit();
     }
 }
